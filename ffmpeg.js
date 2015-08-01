39ae3c4a391bd15bd8dd4b3876ced3084b239493
Dmitriy Savchenko <login.was.here@gmail.com>
2015-08-01 18:44:36 +0300

    Fix sending images as attachments

--- ffmpeg.js
+++ ffmpeg.js
@@ -1,42 +1,74 @@
 var ffmpeg = require('fluent-ffmpeg');
 var request = require('request');
+var moment = require('moment');
 var mailgun = require('mailgun-js')({
     apiKey: process.env.MAILGUN_API_KEY,
     domain: process.env.MAILGUN_DOMAIN
 });
-var writeStream = require('stream').Writable;
 
-var readStream = request(process.env.VIDEO_STREAM_URL);
+var streamBuffers = require("stream-buffers");
+var myWritableStreamBuffer = new streamBuffers.WritableStreamBuffer();
 
-ffmpeg({
-    "source": readStream,
-    "timeout": 0
-})
+var readStream = request(process.env.VIDEO_STREAM_URL)
+    .on('error', function(err) {
+        console.log(err);
+    })
+    .on('start', function(commandLine) {
+        console.log('Spawned Ffmpeg with command: ' + commandLine);
+      })
+    .on('end', function() {
+        console.log('Processing finished !');
+    });
+
+ffmpeg()
+  .input(readStream)
+  .inputFormat('mjpeg')
   .frames(1)
-  .output(writeStream)
-  .on('error', function(err) {
+  .format('mjpeg')
+  .on('error', function(err, stdout, stderr) {
     console.log('An error occurred: ' + err.message);
     console.log(err);
+    console.log('ffmpeg stdout: ' + stdout);
+    console.log('ffmpeg stderr: ' + stderr);
+  })
+  .on('start', function(commandLine) {
+    console.log('Spawned Ffmpeg with command: ' + commandLine);
+  })
+  .on('codecData', function(data) {
+    console.log('Input is ' + data.audio + ' audio ' +
+      'with ' + data.video + ' video');
+  })
+  .on('progress', function(progress) {
+    console.log('Processing: ' + progress.percent + '% done');
   })
   .on('end', function() {
     console.log('Processing finished !');
 
     var date = moment().toISOString();
-    var filename = 'Duet ' + date;
+    var filename = 'Duet_' + date;
+    var filepath = filename + '.jpg';
 
-    var attch = new mailgun.Attachment({data: writeStream, filename: filename});
+    var attachment = new mailgun.Attachment({
+      filename: filepath,
+      data: myWritableStreamBuffer.getContents()
+    });
 
     var data = {
-      from: 'Excited User <me@samples.mailgun.org>',
+      from: process.env.UPLOAD_MAIL_ADDRESS_FROM,
       to: process.env.UPLOAD_MAIL_ADDRESS,
       subject: filename,
       text: 'Image for ' + filename,
-      attachment: attch
+      attachment: attachment
     };
 
     mailgun.messages().send(data, function (error, body) {
+      if (error) {
+        throw error;
+      }
       console.log(body);
+
+      process.exit();
     });
 
   })
-  .run();
+  .pipe(myWritableStreamBuffer, {end: true});
