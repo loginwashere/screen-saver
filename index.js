740de1dbc3858fea52dbd78838e5522deff92d00
Dmitriy Savchenko <login.was.here@gmail.com>
2015-08-02 11:28:44 +0300

    Remove unused code, remove web process

--- index.js
+++ index.js
@@ -1,64 +1,103 @@
-var phantom = require('phantom');
+var ffmpeg = require('fluent-ffmpeg');
+var request = require('request');
+var moment = require('moment');
+var fs = require('fs');
+var Flickr = require("flickrapi");
+var flickrOptions = {
+  api_key: process.env.FLICKR_API_KEY,
+  secret: process.env.FLICKR_API_SECRET,
+  user_id: process.env.FLICKR_USER_ID,
+  access_token: process.env.FLICKR_ACCESS_TOKEN,
+  access_token_secret: process.env.FLICKR_ACCESS_TOKEN_SECRET,
+  permissions: 'write'
+};
 
-phantom.create(function (ph) {
-  ph.createPage(function (page) {
-    page.open("http://37.57.72.244:/snapshot.cgi", function (status) {
-      console.log("opened google? ", status);
-      page.evaluate(
-        function () {
-            console.log(1);
-            return document.title;
-        },
-        function (result) {
-            console.log(2);
-            console.log('Page title is ' + result);
+var streamBuffers = require("stream-buffers");
+var myWritableStreamBuffer = new streamBuffers.WritableStreamBuffer();
 
-          //   Flickr.authenticate(FlickrOptions, function(error, flickr) {
-          //     var uploadOptions = {
-          //       photos: [{
-          //         title: "test",
-          //         tags: [
-          //           "happy fox",
-          //           "test 1"
-          //         ],
-          //         photo: __dirname + "/test.jpg"
-          //       },{
-          //         title: "test2",
-          //         tags: "happy fox image \"test 2\" separate tags",
-          //         photo: __dirname + "/test.jpg"
-          //       }]
-          //     };
+var readStream = request(process.env.VIDEO_STREAM_URL)
+  .on('error', function(err) {
+    console.log(err);
+  })
+  .on('start', function(commandLine) {
+    console.log('Spawned Ffmpeg with command: ' + commandLine);
+  })
+  .on('end', function() {
+    console.log('Processing finished !');
+  });
 
-          //     Flickr.upload(uploadOptions, FlickrOptions, function(err, result) {
-          //       if(err) {
-          //         return console.error(error);
-          //       }
-          //       console.log("photos uploaded", result);
+ffmpeg()
+  .input(readStream)
+  .inputFormat('mjpeg')
+  .frames(1)
+  .format('mjpeg')
+  .on('error', function(err, stdout, stderr) {
+    console.log('An error occurred: ' + err.message);
+    console.log(err);
+    console.log('ffmpeg stdout: ' + stdout);
+    console.log('ffmpeg stderr: ' + stderr);
+  })
+  .on('start', function(commandLine) {
+    console.log('Spawned Ffmpeg with command: ' + commandLine);
+  })
+  .on('codecData', function(data) {
+    console.log('Input is ' + data.audio + ' audio ' +
+      'with ' + data.video + ' video');
+  })
+  .on('progress', function(progress) {
+    console.log('Processing: ' + progress.percent + '% done');
+  })
+  .on('end', function() {
+    console.log('Processing finished !');
 
+    var date = moment().toISOString();
+    var filename = 'Duet_' + date;
+    var filepath = 'photos/' + filename + '.jpg';
 
-          //       ph.exit();
-          //     });
-          //   });
+    var fileContents = myWritableStreamBuffer.getContents();
+
+    var fileReadStream = new streamBuffers.ReadableStreamBuffer();
+    fileReadStream.put(fileContents);
+
+    var localfile = fs.createWriteStream(filepath);
+
+    fileReadStream.pipe(localfile);
+
+    Flickr.authenticate(flickrOptions, function(error, flickr) {
+
+      // we can now use "flickr" as our API object
+      var uploadOptions = {
+        photo: {
+          title: filename,
+          tags: [],
+          photo: filepath
         }
-      );
+      };
 
-      page.render('technews.png');
+      Flickr.upload(uploadOptions, flickr.options, function(err, result) {
+        if(err) {
+          throw err;
+        }
 
-      ph.exit();
+        console.log('result', result);
+
+        var groupsPoolsAddOptions = {
+            user_id: process.env.FLICKR_USER_ID,
+            photo_id: result[0],
+            group_id: process.env.FLICKR_GROUP_ID,
+            format: 'json'
+        };
+
+        flickr.groups.pools.add(groupsPoolsAddOptions, function(err, result) {
+          if(err) {
+            throw err;
+          }
+          console.log('result', result);
+
+          process.exit();
+        });
+      });
     });
 
-    page.onError = function (msg, trace) {
-      console.log(msg);
-      trace.forEach(function(item) {
-        console.log('  ', item.file, ':', item.line);
-      });
-    };
-  });
-});
-
-phantom.onError = function (msg, trace) {
-  console.log(msg);
-  trace.forEach(function(item) {
-    console.log('  ', item.file, ':', item.line);
-  });
-};
+  })
+  .pipe(myWritableStreamBuffer, {end: true});
